23d0e3b6305e6d972b84d1b3f25c72496a76524a
Oscar <odevill@icloud.com>
2025-11-17 20:04:35 +0200

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Oscar De Villiers on 2025/11/17.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
