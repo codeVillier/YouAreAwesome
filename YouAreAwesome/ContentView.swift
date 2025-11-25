e2dc6fd9aa0bb22900f3e02213f08929daec2563
Oscar de Villiers <odevill@icloud.com>
2025-11-25 15:27:56 +0200

    Ch. 1.8 An Into to Types in Swift - SwiftUI course by Prof. Gallaugher

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,42 +10,45 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
     
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: imageString)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
-                .foregroundStyle(.black)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
                 
-                if message == message1 {
-                    message = message2
-                    imageString = imageString2
-                } else {
-                    message = message1
-                    imageString = imageString1
+               message = (message == message1 ? message2 : message1)
+  
+               imageName = "image\(imageNumber)"
+
+               imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
                 }
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
+            
         }
         .padding()
         
