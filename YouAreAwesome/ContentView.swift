53b6e41a017f80a11336bd1fc91844c41f41c27f
Oscar de Villiers <odevill@icloud.com>
2025-11-26 16:08:12 +0200

    Ch. 1.10 .minimumScaleFactor and the SwiftUI animation modifier - Prof Gallaugher's Swift UI Course!

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -17,26 +17,30 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
-            
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
                 .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             Button("Show Message") {
                 let messages = ["Your Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
                                 "You Are Great!",
-                                "Fabulous? That\'s You!",
+                                "Fabulous? That's You!",
                                 "You are Fantastic!",
                                 "I Am Skilled!"]
                 
